bd6c19127763c35c87e3c05e0eddddddda31f4ea
minhoo1505 <teamtheweeknd2016@gmail.com>
2019-10-01 11:20:51 +0700

    ok

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,5 +8,36 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
         }
+
+        public static bool IsOdd(int n)
+        {
+            if(n % 2 != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsEven(int n)
+        {
+            if(n % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1)
+            {
+                return false;
+            }
+            for(int i = 2; i < n; i++)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
